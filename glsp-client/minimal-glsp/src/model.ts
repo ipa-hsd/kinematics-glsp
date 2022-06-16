c1eef12b4b02ea3c024eb3646941b0ea85532515
Harsh Deshpande <harshavardhan.deshpande@ipa.fraunhofer.de>
2022-06-16 18:34:52 +0200

    visualize joint with edge on client side

--- glsp-client/minimal-glsp/src/model.ts
+++ glsp-client/minimal-glsp/src/model.ts
@@ -1,9 +1,9 @@
 /********************************************************************************
- * Copyright (c) 2022 EclipseSource and others.
+ * Copyright (c) 2020-2021 EclipseSource and others.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
- * https://www.eclipse.org/legal/epl-2.0.
+ * http://www.eclipse.org/legal/epl-2.0.
  *
  * This Source Code may also be made available under the following Secondary
  * Licenses when the conditions for such availability set forth in the Eclipse
@@ -13,15 +13,14 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
-package de.fraunhofer.ipa.kinematics.glsp;
 
-import org.eclipse.glsp.graph.DefaultTypes;
+import { SEdge } from '@eclipse-glsp/client';
 
-public final class KinematicsModelTypes {
-   private KinematicsModelTypes() {}
+export class Pose {
+    xyz?: string;
+    rpy?: string;
+}
 
-   public static final String LINK = DefaultTypes.NODE;
-   public static final String JOINT = DefaultTypes.EDGE;
-   public static final String REVOLUTE_JOINT = "joint:revolute";
-
+export class JointEdge extends SEdge {
+    origin?: Pose;
 }
